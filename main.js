1f52ca99a12e4dfee4ac68c3c97d786e86353e40
Terry Romero <terryselt@gmail.com>
2022-03-19 13:45:01 -0400

    arregle el menu1

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// funciones del index
 function comprobation(u, p, message ){
     
     if(u === "admin" && p === "admin"){
@@ -18,3 +19,27 @@ function next(){
 
     let funciona = comprobation(u, p, message)
 }
+
+//funciones del search_ficha_tecnica
+
+function desplegar_menu(){
+    var nav = document.getElementById("search--ficha--nav")
+    var opaco = document.getElementById("search--ficha--color-opaco")
+
+    if(nav.style.display==="none"){
+        nav.style.display="block"
+        opaco.style.display="block"
+    }
+    else{
+        desaparecer()
+    }
+}
+
+function desaparecer(){
+    var nav = document.getElementById("search--ficha--nav")
+    var opaco = document.getElementById("search--ficha--color-opaco")
+
+    nav.style.display="none"
+    opaco.style.display="none"
+}
+
